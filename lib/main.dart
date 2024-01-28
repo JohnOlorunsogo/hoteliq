179d10a5949dc8f24777ee680e2062521feb2e0d
Olorunsogo John Oluwatobi <75017044+JohnOlorunsogo@users.noreply.github.com>
2024-01-28 19:34:59 +0100

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,11 +23,75 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
   @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final PageController _pageController = PageController();
+  var _currentIndex = 0;
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: PageView(
+        controller: _pageController,
+        children: const [
+          Center(
+            child: Text('Page 1'),
+          ),
+          Center(
+            child: Text('Page 2'),
+          ),
+          Center(
+            child: Text('Page 3'),
+          ),
+          Center(
+            child: Text('Page 4'),
+          ),
+        ],
+      ),
+      bottomNavigationBar: SalomonBottomBar(
+        selectedItemColor: Colors.deepPurple,
+        currentIndex: _currentIndex,
+        onTap: (p0) {
+          _pageController.animateToPage(
+            p0,
+            duration: const Duration(milliseconds: 250),
+            curve: Curves.easeInOut,
+          );
+          setState(() {
+            _currentIndex = p0;
+          });
+        },
+        items: [
+          SalomonBottomBarItem(
+            icon: const Icon(Icons.home),
+            title: const Text("Home"),
+          ),
+          SalomonBottomBarItem(
+            icon: const Icon(CupertinoIcons.calendar),
+            title: const Text("Schedule"),
+          ),
+          SalomonBottomBarItem(
+            icon: const Icon(CupertinoIcons.search),
+            title: const Text("Search"),
+          ),
+          SalomonBottomBarItem(
+            icon: const Icon(CupertinoIcons.person),
+            title: const Text("Profile"),
+          ),
+        ],
+      ),
+    );
   }
 }
