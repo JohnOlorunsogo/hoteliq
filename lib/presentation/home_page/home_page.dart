d964e6907127e81f451c626655c2bed1a867fc8c
Olorunsogo John Oluwatobi <75017044+JohnOlorunsogo@users.noreply.github.com>
2024-01-30 20:40:13 +0100

    .

--- lib/presentation/home_page/home_page.dart
+++ lib/presentation/home_page/home_page.dart
@@ -118,6 +118,144 @@ class HomePage extends ConsumerWidget {
                 ],
               ),
             ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Popular Destination",
+                  style: GoogleFonts.plusJakartaSans(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    // color: Colors.grey,
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    'See all',
+                  ),
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Container(
+              height: 110,
+              width: MediaQuery.sizeOf(context).width - 40,
+              padding: const EdgeInsets.symmetric(
+                horizontal: 15,
+                vertical: 10,
+              ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.white,
+              ),
+              child: Row(
+                children: [
+                  Container(
+                    height: 90,
+                    width: 90,
+                    clipBehavior: Clip.antiAliasWithSaveLayer,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: Image.network(
+                      "https://i.pinimg.com/736x/4a/45/22/4a4522f3fafd34b3f286f649373e116d.jpg",
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 16,
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              Text(
+                                "Asteria hotel",
+                                style: GoogleFonts.plusJakartaSans(
+                                  color: const Color(0xFF0F0F0F),
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w700,
+                                  height: 0.11,
+                                ),
+                              ),
+                              const Spacer(),
+                              Row(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Text(
+                                    "\$200,7 ",
+                                    style: GoogleFonts.plusJakartaSans(
+                                      color: const Color(0xFF4C4DDC),
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w700,
+                                      height: 0.11,
+                                    ),
+                                  ),
+                                  Text(
+                                    " /night",
+                                    style: GoogleFonts.plusJakartaSans(
+                                      color: const Color(0xFF878787),
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.w500,
+                                      height: 0.12,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                          // const SizedBox(
+                          //   height: 20,
+                          // ),
+                          Text(
+                            'Wilora NT 0872, Australia',
+                            style: GoogleFonts.plusJakartaSans(
+                              color: const Color(0xFF878787),
+                              fontSize: 12,
+                              fontWeight: FontWeight.w500,
+                              height: 0.12,
+                            ),
+                          ),
+                          // const SizedBox(
+                          //   height: 20,
+                          // ),
+                          Row(
+                            children: [
+                              SizedBox(
+                                child: Row(
+                                  children: List.generate(
+                                    5,
+                                    (index) => const Icon(
+                                      Icons.star,
+                                      color: Colors.amber,
+                                      size: 23,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              const Text("5.0")
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
